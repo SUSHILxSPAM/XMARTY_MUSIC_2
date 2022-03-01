b930ebb3127541c53adc54e2e3af07d276a7b950
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-03-01 08:20:29 +0530

    Update pmpermit.py

--- XmartyMusic/modules/pmpermit.py
+++ XmartyMusic/modules/pmpermit.py
@@ -32,7 +32,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - https://t.me/Xmarty_Support\n\n",
+                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - https://t.me/XCobrAMusic\n\n",
             )
             return
 
