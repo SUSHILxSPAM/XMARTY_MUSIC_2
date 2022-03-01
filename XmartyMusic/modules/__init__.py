e84c3af46aa19007787a53304028556f2609edd0
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-03-01 08:03:48 +0530

    Update __init__.py

--- XmartyMusic/modules/__init__.py
+++ XmartyMusic/modules/__init__.py
@@ -1,4 +1,4 @@
-# XmartyMusic (Telegram bot project .)
+# XcobrAMusic (Telegram bot project .)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
