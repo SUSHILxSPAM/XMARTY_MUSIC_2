406bb87f70cfbe42f9b665b3382fe0063a309db6
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-03-01 08:00:05 +0530

    Update admins.py

--- XmartyMusic/function/admins.py
+++ XmartyMusic/function/admins.py
@@ -1,5 +1,5 @@
-# XmartyMusic (Telegram bot project )
-# Copyright (C) 2021  Xmartperson
+# XCobraMusic (Telegram bot project )
+# Copyright (C) 2021  XCobraMusic
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
