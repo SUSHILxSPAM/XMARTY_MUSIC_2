cba3a4ebc87948c90c50f433b9eca121165c8797
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-03-01 07:59:31 +0530

    Update __init__.py

--- XmartyMusic/function/__init__.py
+++ XmartyMusic/function/__init__.py
@@ -1,5 +1,5 @@
-# XmartyMuisc (Telegram bot project )
-# Copyright (C) 2021  Xmartperson
+# XCobraMuisc (Telegram bot project )
+# Copyright (C) 2021  Sushil8xop
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from XmartyMusic.function.admins import admins, get, set
+from XcobraMUSIC.function.admins import admins, get, set
 
 __all__ = ["set", "get", "admins"]
