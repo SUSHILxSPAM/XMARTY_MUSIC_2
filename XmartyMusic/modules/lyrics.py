f1ba484e8c5466b221f0e2a72c4a4d7cab7520ed
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-03-01 08:06:06 +0530

    Update lyrics.py

--- XmartyMusic/modules/lyrics.py
+++ XmartyMusic/modules/lyrics.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2021 TeamXmarty
+# Copyright (C) 2021 TeamXcobra
 
 
-# This file is part of Xmarty (Telegram Bot)
+# This file is part of XCobrA (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
